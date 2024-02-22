2ef45bf5db708b09baa4bc70711c55f8592bb310
Lennard Scheffler <lennard.scheffler@e-c-crew.de>
2024-02-22 11:02:28 +0100

    Hotfix: Incoming Booking Tenant Mail

--- src/platform/api/controllers/checkout-controller.js
+++ src/platform/api/controllers/checkout-controller.js
@@ -137,7 +137,7 @@ class CheckoutController {
 
         try {
           await MailController.sendIncomingBooking(
-            booking.mail,
+            tenant.mail,
             booking.id,
             booking.tenant,
           );
