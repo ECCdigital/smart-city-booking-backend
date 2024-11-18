67975b768c06595d691528f30bd4e50a62a58176
Lennard Scheffler <lennard.scheffler@e-c-crew.de>
2024-11-18 15:55:36 +0100

    Fixed Formatting

--- src/platform/exporters/controllers/csv-export-controller.js
+++ src/platform/exporters/controllers/csv-export-controller.js
@@ -7,8 +7,8 @@ const EventManager = require("../../../commons/data-managers/event-manager");
 const bunyan = require("bunyan");
 
 const logger = bunyan.createLogger({
-    name: "csv-export-controller.js",
-    level: process.env.LOG_LEVEL,
+  name: "csv-export-controller.js",
+  level: process.env.LOG_LEVEL,
 });
 
 class CsvExportController {
@@ -53,70 +53,80 @@ class CsvExportController {
   }
 
   static async getEventBookings(request, response) {
-      try {
-          const {
-              params: {tenant: tenantId, id: eventId},
-              user,
-          } = request;
+    try {
+      const {
+        params: { tenant: tenantId, id: eventId },
+        user,
+      } = request;
 
-            const event = await EventManager.getEvent(eventId, tenantId);
-            if (!(await CsvExportController._hasPermission(event, user?.id, tenantId))) {
-              return response.sendStatus(403);
-            }
-
-            const eventBookings = await BookingManager.getEventBookings(tenantId, eventId);
-
-            const attendeeList = eventBookings.map((b) => {
-                return {
-                    id: b.id,
-                    ticket: b.bookableItems.length > 0 ? b.bookableItems[0]._bookableUsed?.title : "Unbekannt",
-                    amount: b.bookableItems.length > 0 ? b.bookableItems[0].amount : "0",
-                    assignedUserId: b.assignedUserId,
-                    mail: b.mail,
-                    company: b.company,
-                    name: b.name,
-                    street: b.street,
-                    zipCode: b.zipCode,
-                    location: b.location,
-                    comment: b.comment?.replace(/(\r\n|\n|\r)/gm, " "),
-                    timeBegin: Formatters.formatDateTime(b.timeBegin),
-                    timeEnd: Formatters.formatDateTime(b.timeEnd),
-                    timeCreated: Formatters.formatDateTime(b.timeCreated),
-                    isCommitted: b.isCommitted ? "Ja" : "Nein",
-                    isPayed: b.isPayed ? "Ja" : "Nein",
-                    priceEur: Formatters.formatCurrency(b.priceEur),
-                    payMethod: Formatters.translatePayMethod(b.payMethod),
-                  };
-          });
-
-            // Using the BOM character to ensure that Excel opens the CSV file correctly on Windows Machines
-          const csvResult = '\uFEFF' + CsvExportController._toCsv(attendeeList, {
-              id: "Buchungsnummer",
-              ticket: "Ticket",
-              amount: "Anzahl",
-              assignedUserId: "Angemeldeter Benutzer",
-              mail: "E-Mail Adresse",
-              company: "Firma",
-              name: "Name",
-              street: "Straße",
-              zipCode: "PLZ",
-              location: "Ort",
-              comment: "Buchungshinweise",
-              timeBegin: "Buchungsbeginn",
-              timeEnd: "Buchungsende",
-              timeCreated: "Buchungsdatum",
-              isCommitted: "Buchung bestätigt",
-              isPayed: "Buchung bezahlt",
-              priceEur: "Preis",
-              payMethod: "Zahlungsart",
-          });
-
-          response.setHeader("Content-Type", "text/csv");
-          response.status(200).send(csvResult);
-      } catch (err) {
-          logger.error(err);
-          response.status(500).send("could not get event bookings");
+      const event = await EventManager.getEvent(eventId, tenantId);
+      if (
+        !(await CsvExportController._hasPermission(event, user?.id, tenantId))
+      ) {
+        return response.sendStatus(403);
       }
+
+      const eventBookings = await BookingManager.getEventBookings(
+        tenantId,
+        eventId,
+      );
+
+      const attendeeList = eventBookings.map((b) => {
+        return {
+          id: b.id,
+          ticket:
+            b.bookableItems.length > 0
+              ? b.bookableItems[0]._bookableUsed?.title
+              : "Unbekannt",
+          amount: b.bookableItems.length > 0 ? b.bookableItems[0].amount : "0",
+          assignedUserId: b.assignedUserId,
+          mail: b.mail,
+          company: b.company,
+          name: b.name,
+          street: b.street,
+          zipCode: b.zipCode,
+          location: b.location,
+          comment: b.comment?.replace(/(\r\n|\n|\r)/gm, " "),
+          timeBegin: Formatters.formatDateTime(b.timeBegin),
+          timeEnd: Formatters.formatDateTime(b.timeEnd),
+          timeCreated: Formatters.formatDateTime(b.timeCreated),
+          isCommitted: b.isCommitted ? "Ja" : "Nein",
+          isPayed: b.isPayed ? "Ja" : "Nein",
+          priceEur: Formatters.formatCurrency(b.priceEur),
+          payMethod: Formatters.translatePayMethod(b.payMethod),
+        };
+      });
+
+      // Using the BOM character to ensure that Excel opens the CSV file correctly on Windows Machines
+      const csvResult =
+        "\uFEFF" +
+        CsvExportController._toCsv(attendeeList, {
+          id: "Buchungsnummer",
+          ticket: "Ticket",
+          amount: "Anzahl",
+          assignedUserId: "Angemeldeter Benutzer",
+          mail: "E-Mail Adresse",
+          company: "Firma",
+          name: "Name",
+          street: "Straße",
+          zipCode: "PLZ",
+          location: "Ort",
+          comment: "Buchungshinweise",
+          timeBegin: "Buchungsbeginn",
+          timeEnd: "Buchungsende",
+          timeCreated: "Buchungsdatum",
+          isCommitted: "Buchung bestätigt",
+          isPayed: "Buchung bezahlt",
+          priceEur: "Preis",
+          payMethod: "Zahlungsart",
+        });
+
+      response.setHeader("Content-Type", "text/csv");
+      response.status(200).send(csvResult);
+    } catch (err) {
+      logger.error(err);
+      response.status(500).send("could not get event bookings");
+    }
   }
 }
 
