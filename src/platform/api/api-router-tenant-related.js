f5adfac3419a6697fbffa4d330ff4298b377358c
Marvin Anders <marvin.anders@e-c-crew.de>
2025-01-29 13:02:56 +0100

    testing

--- src/platform/api/api-router-tenant-related.js
+++ src/platform/api/api-router-tenant-related.js
@@ -179,6 +179,7 @@ router.post("/checkout/validateItem", CheckoutController.validateItem);
 // Public
 router.post("/payments", PaymentController.createPayment);
 router.get("/payments/notify", PaymentController.paymentNotification);
+router.post("/payments/notify", PaymentController.paymentNotification);
 router.post("/payments/response", PaymentController.paymentResponse);
 router.get("/payments/response", PaymentController.paymentResponse);
 
