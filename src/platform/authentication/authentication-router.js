919876e95c6f49a75219673c8ac3cc6fded0ffb6
Marvin Anders <marvin.anders@e-c-crew.de>
2025-06-24 09:38:30 +0200

    added checkEmail route and improved singup response

--- src/platform/authentication/authentication-router.js
+++ src/platform/authentication/authentication-router.js
@@ -10,12 +10,24 @@ router.post("/signup", AuthenticationController.signup);
 router.get("/verify/:hookId", AuthenticationController.releaseHook);
 router.get("/reset/:hookId", AuthenticationController.releaseHook);
 router.post("/resetpassword", AuthenticationController.resetPassword);
+router.post("/check-email", AuthenticationController.checkEmail);
 
-router.post(
-  "/signin",
-  passport.authenticate("local-signin"),
-  AuthenticationController.signin,
-);
+router.post("/signin", (req, res, next) => {
+  passport.authenticate("local-signin", (err, user, info) => {
+    if (err) {
+      return res.status(err.status).json({ message: err.message });
+    }
+    if (!user) {
+      return res.status(401).json({ message: "Authentication failed" });
+    }
+    req.login(user, (err) => {
+      if (err) {
+        return next(err);
+      }
+      return AuthenticationController.signin(req, res, next);
+    });
+  })(req, res, next);
+});
 router.post("/sso/signin", AuthenticationController.ssoLogin);
 router.post("/sso/signup", AuthenticationController.ssoSignup);
 router.get(
