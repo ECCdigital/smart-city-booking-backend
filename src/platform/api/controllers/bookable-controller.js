5bb82cc9f7a213d0176457bc0ca52813c76acd47
kpw89 <kim.westphalen@googlemail.com>
2025-11-10 14:57:11 +0100

    added bookable back as fallback when existingBookable does not exists (during create)

--- src/platform/api/controllers/bookable-controller.js
+++ src/platform/api/controllers/bookable-controller.js
@@ -329,7 +329,7 @@ class BookableController {
 
       if (
         await PermissionService._allowUpdate(
-          existingBookable,
+          existingBookable || bookable,
           user.id,
           tenant,
           RolePermission.MANAGE_BOOKABLES,
