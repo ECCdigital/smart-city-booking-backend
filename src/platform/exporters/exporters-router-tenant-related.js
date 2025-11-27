654679673a57a6fd7263b01e01d06df3a8848858
Marvin Anders <marvin.anders@e-c-crew.de>
2025-11-27 12:52:07 +0100

    solved issue with csv export

--- src/platform/exporters/exporters-router-tenant-related.js
+++ src/platform/exporters/exporters-router-tenant-related.js
@@ -1,7 +1,12 @@
 const express = require("express");
 const CsvExportController = require("./controllers/csv-export-controller");
+const AuthenticationController = require("../authentication/controllers/authentication-controller");
 const router = express.Router({ mergeParams: true });
 
-router.get("/events/:id/bookings", CsvExportController.getEventBookings);
+router.get(
+  "/events/:id/bookings",
+  AuthenticationController.isSignedIn,
+  CsvExportController.getEventBookings,
+);
 
 module.exports = router;
